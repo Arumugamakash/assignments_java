42c8744b223351d36fca1d9c7c1753c5c63aa21b
Arumugamakash <142109407+Arumugamakash@users.noreply.github.com>
2024-05-19 20:22:03 +0530

    Update Qn4.java

--- assignment17/Qn4.java
+++ assignment17/Qn4.java
@@ -1,75 +1,123 @@
 package assignment17;
 
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 /*
- 4. Simulate a Music Player Playlist. Create a chain of songs in LinkedList. 
- The Head points to the current song. Add a provision to add, remove songs anywhere in the playlist.
+ * 4. Simulate a Music Player Playlist. Create a chain of songs in LinkedList.
+ * The Head points to the current song. Add a provision to add, remove songs
+ * anywhere in the playlist.
  */
+
+class Song {
+	String title;
+
+	public Song(String title) {
+		this.title = title;
+	}
+}
+
 public class Qn4 {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Queue<String> song = new LinkedList<String>();
-		start(song);
+		System.out.println("Welcome to Music Player");
+		getInstance();
+		init();
 	}
 
-	private static void start(Queue<String> song) {
-		System.out.println(
-				"\n1.View my playList\n2.AddSong\n3.Remove Song\n4.Current Playing Song\n5.Exit\nEnter Your Choice");
+	private static MusicPlayerPlaylist playlist;
+
+	static MusicPlayerPlaylist getInstance() {
+		if (playlist == null) {
+			playlist = new MusicPlayerPlaylist();
+		}
+		return playlist;
+	}
+
+	public static void init() {
+		System.out.println("\n 1.Add Song  \n 2.Current Song \n 3.Remove Song \n 4.View my Playlist \n 5.Exit");
+		System.out.println("Enter your Choice");
 		int choice = sc.nextInt();
 		sc.nextLine();
 		switch (choice) {
 		case 1:
-			if (song.isEmpty()) {
-				System.out.println("No song Added");
-			} else {
-				System.out.println(song);
-			}
-			start(song);
+			playlist.addSong();
+			init();
 			break;
 		case 2:
-			System.out.println("Enter Your Song");
-			song.add(sc.nextLine());
-			System.out.println("Song Added Successsfully");
-			start(song);
-			break;
-		case 3:
-			removeSong(song);
-			start(song);
+			playlist.playCurrentSong();
+			init();
 			break;
 		case 4:
-			System.out.println("Current Playing song :" + song.peek());
-			start(song);
+			playlist.removeSong();
+			init();
 			break;
 		case 5:
-			System.out.println("Than you for Using");
+			playlist.printPlaylist();
+			init();
 			break;
-
 		default:
+			System.out.println("Invalid Input");
 			break;
 		}
 	}
+}
 
-	private static void removeSong(Queue<String> song) {
-		System.out.println("Enter Your deleted song name");
-		String name = sc.nextLine();// anbe
-		boolean flag = true;
+class MusicPlayerPlaylist {
+	Scanner sc = new Scanner(System.in);
+	LinkedList<Song> playlist;
+	Song currentSong;
 
-		Iterator<String> d1 = song.iterator();
-		while (d1.hasNext()) {
-			if (d1.next().equals(name)) {
-				d1.remove();
-				System.out.println("Song has removed");
-				flag = false;
-				break;
+	public MusicPlayerPlaylist() {
+		playlist = new LinkedList<>();
+		currentSong = null;
+	}
+
+	// add song
+	public void addSong() {
+		System.out.println("Enter the name of the song");
+		String name = sc.nextLine();
+		Song s = new Song(name);
+		playlist.add(s);
+		System.out.println(playlist);
+		if (currentSong == null) {
+			currentSong = s;
+		}
+		System.out.println("Song Added Successfully");
+	}
+
+	// remove song
+	public void removeSong() {
+		System.out.println("Enter the name of the song");
+		String name = sc.nextLine();
+		if (playlist.isEmpty()) {
+			System.out.println("Playlist is empty.");
+			return;
+		}
+		for (Song song : playlist) {
+			if (song.title.equals(name)) {
+				playlist.remove(song);
+				System.out.println("Song Removed Successfully");
+				return;
 			}
 		}
-		if (flag) {
-			System.out.println("Song not found");
+
+	}
+
+	// current song
+	public void playCurrentSong() {
+		if (currentSong == null) {
+			System.out.println("No song is currently selected.");
+		} else {
+			System.out.println("Playing: " + currentSong.title);
+		}
+	}
+
+	// entire playList
+	public void printPlaylist() {
+		for (Song song : playlist) {
+			System.out.println(song.title);
 		}
 	}
 }
